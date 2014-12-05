7eec8b289493d06eda9b83c090210c5901660f48
Korroz <korroz.with.git@voidentity.net>
2014-12-05 01:39:23 +0100

    Draw image onto canvas

--- src/components/cr-cata/cr-cata.directive.js
+++ src/components/cr-cata/cr-cata.directive.js
@@ -1,16 +1,30 @@
 'use strict';
 
 angular.module('circlr')
-  .directive('crCata', function ($log) {
+  .directive('crCata', function ($log, $window) {
     return {
       restrict: 'E',
       template: '<canvas></canvas>',
       link: function (scope, el, attrs) {
-        $log.log(el);
+        $log.log('cr-cata element:', el);
         var canvas = el[0].firstElementChild;
         var ctx = canvas.getContext('2d');
         ctx.fillStyle = 'green';
         ctx.fillRect(10, 10, 100, 100);
+
+        scope.$watch('imageUrl', function (newUrl, oldUrl) {
+          $log.log("cr-cata watch imageUrl:", newUrl);
+          if (!newUrl)
+            return;
+
+          var img = new $window.Image();
+          img.onload = function () {
+            canvas.height = img.naturalHeight;
+            canvas.width = img.naturalWidth;
+            ctx.drawImage(img, 0, 0);
+          };
+          img.src = newUrl;
+        });
       }
     };
   });
