5d8ec77a79cf89b73c77f42b10b39aa9eb6e49af
Korroz <korroz.with.git@voidentity.net>
2014-12-28 00:58:57 +0100

    Tweak transitions for viewBox in cr-player

--- src/components/cr-circlr/cr-player/cr-player.directive.js
+++ src/components/cr-circlr/cr-player/cr-player.directive.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('crCirclr')
-  .directive('crPlayer', function (d3, $timeout) {
+  .directive('crPlayer', function (d3, $timeout, $window, $log) {
     return {
       restrict: 'E',
       templateUrl: 'components/cr-circlr/cr-player/cr-player.html',
       scope: {},
-      controller: function ($scope, $window) {
+      controller: function ($scope) {
         $scope.series = $window.JSON.parse($window.localStorage.getItem('circlrSeries'));
       },
       link: function (scope, el) {
@@ -14,24 +14,28 @@ angular.module('crCirclr')
 
         var fireFrame = function (payload, index) {
           $timeout(function () {
+            $log.debug('cr-player playing frame', payload);
             var pl = payload;
+            var hc = pl.columns / 2, hr = pl.rows / 2;
 
-            svg.transition().attr('viewBox', '0 0 ' + pl.columns + ' ' + pl.rows);
+            svg.transition().attr('viewBox', '' + (-hc) + ' ' + (-hr) + ' ' + pl.columns + ' ' + pl.rows);
 
             var c = svg.selectAll('circle').data(pl.points.filter(function (p) { return p.l > pl.settings.cutoff; }));
 
             c.enter().append('circle')
-              .attr('cx', function () { return Math.random() * pl.columns; })
-              .attr('cy', function () { return Math.random() * pl.rows; })
+              .attr('cx', function () { return Math.random() * pl.columns - hc; })
+              .attr('cy', function () { return Math.random() * pl.rows - hr; })
               .attr('r', 0);
 
             c.transition().delay(250).duration(2000)
-              .attr('cx', function (d) { return d.x + 0.5; })
-              .attr('cy', function (d) { return d.y + 0.5; })
+              .attr('cx', function (d) { return d.x + 0.5 - hc; })
+              .attr('cy', function (d) { return d.y + 0.5 - hr; })
               .attr('fill', function (d) { return d.color; })
               .attr('r', function (d) { return d.l * 0.7; });
 
-            c.exit().remove();
+            c.exit().transition().duration(2000)
+              .attr('r', 0)
+              .remove();
           }, 5000 * index, false);
         };
 
